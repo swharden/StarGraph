9a191a0e35c7e9030e8de2cb215958b56c2f81a8
Scott W Harden <swharden@gmail.com>
2021-05-24 21:43:05 -0400

    add methods to convert to/from JSON

--- src/StarGraph/GitHubJSON.cs
+++ src/StarGraph/GitHubJSON.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
@@ -16,6 +17,45 @@ namespace StarGraph
                 {
                     DateTime = DateTime.Parse(x.GetProperty("starred_at").GetString()),
                     User = x.GetProperty("user").GetProperty("login").GetString()
+                })
+                .ToArray();
+        }
+
+        public static string RecordsToJson(StarRecord[] records)
+        {
+            using var stream = new MemoryStream();
+            var options = new JsonWriterOptions() { Indented = true };
+            using var writer = new Utf8JsonWriter(stream, options);
+
+            writer.WriteStartObject();
+            writer.WriteString("lastUpdated", DateTime.UtcNow);
+            writer.WriteStartArray("starRecords");
+            foreach (var record in records)
+            {
+                writer.WriteStartObject();
+                writer.WriteString("utc", record.DateTime.ToUniversalTime());
+                writer.WriteString("user", record.User);
+                writer.WriteEndObject();
+            }
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+
+            writer.Flush();
+            string json = Encoding.UTF8.GetString(stream.ToArray());
+
+            return json;
+        }
+
+        public static StarRecord[] RecordsFromJson(string json)
+        {
+            using JsonDocument document = JsonDocument.Parse(json);
+            return document.RootElement
+                .GetProperty("starRecords")
+                .EnumerateArray()
+                .Select(x => new StarRecord()
+                {
+                    User = x.GetProperty("user").GetString(),
+                    DateTime = DateTime.Parse(x.GetProperty("utc").GetString())
                 })
                 .ToArray();
         }
---
+++ src/StarGraphTests/JsonTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace StarGraphTests
+{
+    class JsonTests
+    {
+        [Test]
+        public void Test_ToJson_DoesNotFail()
+        {
+            StarGraph.StarRecord[] records = SampleData.GetAllStargazers();
+            string json = StarGraph.GitHubJSON.RecordsToJson(records);
+            Console.WriteLine(json);
+            Assert.IsNotNull(json);
+            Assert.Greater(json.Length, 100);
+        }
+
+        [Test]
+        public void Test_FromJson_MatchesToJson()
+        {
+            StarGraph.StarRecord[] records = SampleData.GetAllStargazers();
+            string json = StarGraph.GitHubJSON.RecordsToJson(records);
+
+            StarGraph.StarRecord[] records2 = StarGraph.GitHubJSON.RecordsFromJson(json);
+            Assert.IsNotNull(records2);
+            Assert.AreEqual(records.Length, records2.Length);
+
+            for (int i = 0; i < records.Length; i++)
+            {
+                Assert.AreEqual(records[i].User, records2[i].User);
+                Assert.AreEqual(records[i].DateTime, records2[i].DateTime);
+            }
+        }
+    }
+}
