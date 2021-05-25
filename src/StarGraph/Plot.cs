89b591d1c176893f274ac6cc963f2f0f46026e3d
Scott W Harden <swharden@gmail.com>
2021-05-24 21:02:44 -0400

    show UTC timestamp on plot

--- src/StarGraph/Plot.cs
+++ src/StarGraph/Plot.cs
@@ -12,18 +12,26 @@ namespace StarGraph
         {
             double[] xs = records.Select(x => x.DateTime.ToOADate()).ToArray();
             double[] ys = ScottPlot.DataGen.Consecutive(records.Length, offset: 1);
-            string day = DateTime.Now.ToString("yyyy-MM-dd");
-            string msg = "Recent Stargazers:\n" + string.Join("\n", records.TakeLast(12).Select(x => " - " + x.User));
 
             var plt = new ScottPlot.Plot(600, 350);
             plt.AddScatterLines(xs, ys, lineWidth: 2);
 
-            var annotation = plt.AddAnnotation(msg, 10, 10);
-            annotation.BackgroundColor = ColorTranslator.FromHtml("#F6F6F6");
+            string msg = "Recent Stargazers:\n" + string.Join("\n", records.TakeLast(12).Select(x => " - " + x.User));
+            var starsAnnotation = plt.AddAnnotation(msg, 10, 10);
+            starsAnnotation.BackgroundColor = ColorTranslator.FromHtml("#F6F6DD");
+
+            string day = DateTime.UtcNow.ToString("yyyy-MM-dd");
+            string time = DateTime.UtcNow.ToString("H:mm");
+            var dateAnnotation = plt.AddAnnotation($"updated {day} at {time} UTC", -3, -1);
+            dateAnnotation.Border = false;
+            dateAnnotation.Shadow = false;
+            dateAnnotation.Background = false;
+            dateAnnotation.Font.Color = Color.FromArgb(150, Color.Black);
+            dateAnnotation.Font.Name = ScottPlot.Drawing.InstalledFont.Monospace();
+            dateAnnotation.Font.Size = 10;
 
             plt.Title($"ScottPlot has {records.Length} GitHub Stars");
             plt.YLabel("Total Stars");
-            plt.YAxis.SetSizeLimit(min: 50);
             plt.XAxis.DateTimeFormat(true);
 
             Bitmap bmp = plt.Render();
