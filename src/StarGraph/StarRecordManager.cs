a70acb6a4992e18ba04f440b69420f1959179a59
Scott W Harden <swharden@gmail.com>
2021-05-24 22:26:52 -0400

    create record manager

---
+++ src/StarGraph/StarRecordManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarGraph
+{
+    public class StarRecordManager
+    {
+        public readonly Dictionary<string, DateTime> records = new();
+        public int Count => records.Count;
+
+        public StarRecordManager()
+        {
+
+        }
+
+        public void TryAdd(StarRecord record)
+        {
+            records.TryAdd(record.User, record.DateTime);
+        }
+
+        public void TryAdd(StarRecord[] records)
+        {
+            foreach (StarRecord record in records)
+                TryAdd(record);
+        }
+    }
+}
---
+++ src/StarGraphTests/StarRecordManagerTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StarGraphTests
+{
+    class StarRecordManagerTests
+    {
+        [Test]
+        public void Test_RecordManager_IgnoresDuplicateRecords()
+        {
+            StarGraph.StarRecord[] allRecords = SampleData.GetAllStargazers();
+
+            var stars = new StarGraph.StarRecordManager();
+            Assert.AreEqual(0, stars.Count);
+
+            stars.TryAdd(allRecords.Take(100).ToArray());
+            Assert.AreEqual(100, stars.Count);
+
+            stars.TryAdd(allRecords.Skip(50).Take(100).ToArray());
+            Assert.AreEqual(150, stars.Count);
+        }
+    }
+}
