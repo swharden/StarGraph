4bed4c54686145477539edd6cb7e121831bae350
Scott W Harden <swharden@gmail.com>
2021-05-24 23:18:46 -0400

    create authentication test module

---
+++ src/StarGraphTests/Authentication.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarGraphTests
+{
+    public static class Authentication
+    {
+        public static string GetGitHubAccessToken()
+        {
+            var appConfig = new ConfigurationBuilder().AddUserSecrets<GitHubApiTests>().Build();
+            string token = appConfig["github-token-starchart-readuser"];
+            if (string.IsNullOrWhiteSpace(token))
+                throw new InvalidOperationException("token not found");
+            return token;
+        }
+    }
+}
